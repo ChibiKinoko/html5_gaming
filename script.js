dccee293b0eaee73529e1a3676e21af2ca9bcf82
Laure <laure.meng@gmail.com>
2015-11-20 00:11:45 +0100

    appararition boss

--- script.js
+++ script.js
@@ -10,13 +10,19 @@ var enemys = new Array();
 var lastShoot = 0;
 var lastEnemyDisplay = 0;
 
-var score = 0;
+var score;
 var scoreText;
 var finalScore;
 var textFail;
 
 var bulletEnemy;
 var bulletsEnemy = new Array();
+
+var bossImg;
+var boss;
+var boss2;
+var boss3;
+var bossBullets = new Array();
 
 var lifeScore = 3;
 var lifeArray = new Array();
@@ -29,6 +35,8 @@ var myVar;
 var level = 0;
 var levelVitesse = 3000;
 var textLevel;
+
+var flag = false;
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
@@ -86,11 +94,23 @@ function load() {
 
     //ENEMY
     enemyImg = new Image();
-    enemyImg.src = 'img/enemy.png';
+    enemyImg.src = 'img/enemy2.png';
 
     //BULLETS ENEMY
     bulletEnemyImg = new Image();
     bulletEnemyImg.src = 'img/bulletEnemy.png';
+
+    //BOSS
+    bossImg = new Image();
+    bossImg.src = 'img/boss.gif';
+    boss = new createjs.Bitmap(bossImg);
+    boss2 = new createjs.Bitmap(bossImg);
+    boss3 = new createjs.Bitmap(bossImg);
+    // boss.x = 225;
+    // boss2.y = 88;
+    // boss2.x = 137;
+    // boss3.y = 88;
+    // boss3.x = 313;
 
     //SCORE
     score = 0;
@@ -142,7 +162,7 @@ function load() {
 };
 
 function startGame() {
-    //console.log(levelVitesse);
+
     levels();
 
     shoot();
@@ -181,6 +201,144 @@ function shoot() {
     };
 }
 
+function addBulletEnemy() {
+
+    myVar = setInterval(function () {
+        for (var a = 0; a < enemys.length; a++) {
+            bulletEnemy = new createjs.Bitmap(bulletEnemyImg);
+            bulletsEnemy.push(bulletEnemy);
+
+            stage.addChild(bulletEnemy);
+            bulletEnemy.x = enemys[a].x + 15;
+            bulletEnemy.y = enemys[a].y + 50;
+        };
+    }, 2000);
+}
+
+function levels() {
+    if (score == 0) {
+
+        stage.removeChild(textLevel);
+        textLevel = new createjs.Text("Level 1", "50px Verdana", "#FFFFFF");
+
+        levelVitesse = 3000;
+        stage.addChild(textLevel);
+
+    } else if (score == 500) {
+        stage.removeChild(textLevel);
+        textLevel = new createjs.Text("Level 2", "50px Verdana", "#FFFFFF");
+
+        levelVitesse = 2000;
+        stage.addChild(textLevel);
+
+    } else if (score == 1000) {
+        stage.removeChild(textLevel);
+        textLevel = new createjs.Text("Level 3", "50px Verdana", "#FFFFFF");
+
+        levelVitesse = 900
+        stage.addChild(textLevel);
+
+    } else if (score == 1300) {
+
+        stage.removeChild(textLevel);
+        textLevel = new createjs.Text("BOSS", "50px Verdana", "#FFFFFF");
+        stage.addChild(textLevel);
+
+        enemys = [];
+        bulletsEnemy = [];
+        createjs.Ticker.removeEventListener("tick", startGame);
+        clearInterval(myVar);
+
+        setTimeout(function () {
+        stage.removeChild();
+        stage.addChild(bg, bg2, scoreText, ship);
+            createjs.Ticker.addEventListener("tick", displayBoss);
+        }, 2000);
+    }
+
+    // stage.addChild(textLevel);
+    textLevel.x = 200;
+    textLevel.y = 300;
+
+    // setTimeout(function () {
+    //     //console.log('toto');
+    //     stage.removeChild(textLevel);
+    //     stage.update();
+    // }, 1000);
+}
+
+function displayEnemy() {
+
+    //console.log(levelVitesse);
+
+    var random = Math.floor(Math.random() * (550 + 1));
+
+    if (Date.now() - lastEnemyDisplay > levelVitesse) {
+        lastEnemyDisplay = Date.now();
+
+        var enemy = new createjs.Bitmap(enemyImg);
+        enemys.push(enemy);
+
+        stage.addChild(enemy); //ajout des enemy
+        enemy.x = random;
+    }
+
+    bulletsEnemy.forEach(function (bulletEnemy) { //deplacement des bullets enemy
+        bulletEnemy.y += 10;
+    });
+}
+
+function displayBoss() {
+
+    stage.addChild(boss, boss2, boss3);
+    boss.x = 225;
+    boss2.y = 88;
+    boss2.x = 137;
+    boss3.y = 88;
+    boss3.x = 313;
+    stage.update();
+}
+
+function collisions() {
+    for (var j = 0; j < enemys.length; j++) {
+
+        enemys[j].y += 3;
+
+        if (enemys[j].y >= 851) { //suppression enemy en dehors du canvas
+            stage.removeChild(enemys[j]);
+            enemys.splice(j, 1);
+        };
+
+        for (var k = 0; k < bullets.length; k++) { // collision tirs allies
+            if (typeof enemys[j] !== "undefined" && bullets[k].x >= enemys[j].x && bullets[k].x + 11 < enemys[j].x + 49 && bullets[k].y < enemys[j].y + 40) {
+
+                scoreUpdate();
+
+                stage.removeChild(enemys[j]); //suppresion de l'enemis et de la bullet qui le touche`
+                enemys.splice(j, 1);
+
+                stage.removeChild(bullets[k]);
+                bullets.splice(k, 1);
+            };
+        };
+
+        for (var m = 0; m < bulletsEnemy.length; m++) { // collision tirs enemy
+
+            if (bulletsEnemy[m].x >= ship.x && bulletsEnemy[m].x + 11 < ship.x + 37 && bulletsEnemy[m].y > ship.y) {
+                //console.log('touche');
+                stage.removeChild(bulletsEnemy[m]); //supression de la bullet qui m'a touche
+                bulletsEnemy.splice(m, 1);
+
+                failed();
+            };
+            if (typeof bulletsEnemy[m] !== "undefined" && bulletsEnemy[m].y >= 851) {
+                stage.removeChild(bulletsEnemy[m]);
+                bulletsEnemy.splice(m, 1);
+            };
+        };
+    };
+}
+
 function failed() {
     textFail = new createjs.Text("Perdu !", "40px Verdana", "#FFFFFF");
     finalScore = new createjs.Text("Score final : " + score, "30px Verdana", "#FFFFFF");
@@ -205,108 +363,5 @@ function failed() {
         finalScore.y = 300;
 
         clearInterval(myVar);
-    };
-}
-
-function addBulletEnemy() {
-
-    myVar = setInterval(function () {
-        for (var a = 0; a < enemys.length; a++) {
-            bulletEnemy = new createjs.Bitmap(bulletEnemyImg);
-            bulletsEnemy.push(bulletEnemy);
-
-            stage.addChild(bulletEnemy);
-            bulletEnemy.x = enemys[a].x + 20;
-            bulletEnemy.y = enemys[a].y + 50;
-        };
-    }, 2000);
-}
-
-function levels() {
-    if (score >= 0 && score < 500) {
-        //console.log('lvl 1');
-        textLevel = new createjs.Text("Level 1", "50px Verdana", "#FFFFFF");
-        levelVitesse = 3000;
-
-    } else if (score >= 500 && score < 1000) {
-        //console.log('lvl 2');
-        textLevel = new createjs.Text("Level 2", "50px Verdana", "#FFFFFF");
-        levelVitesse = 2000;
-
-    } else if (score => 1000 && score < 1300) {
-        //console.log('lvl 3');
-        textLevel = new createjs.Text("Level 3", "50px Verdana", "#FFFFFF");
-        levelVitesse = 900;
-    };
-
-    stage.addChild(textLevel);
-    textLevel.x = 200;
-    textLevel.y = 300;
-
-    setTimeout(function () {
-        //console.log('toto');
-        stage.removeChild(textLevel);
-        stage.update();
-    }, 1000);
-}
-
-function displayEnemy() {
-
-    //console.log(levelVitesse);
-
-    var random = Math.floor(Math.random() * (550 + 1));
-
-    if (Date.now() - lastEnemyDisplay > levelVitesse) {
-        lastEnemyDisplay = Date.now();
-
-        var enemy = new createjs.Bitmap(enemyImg);
-        enemys.push(enemy);
-
-        stage.addChild(enemy); //ajout des enemy
-        enemy.x = random;
-    }
-
-    bulletsEnemy.forEach(function (bulletEnemy) { //deplacement des bullets enemy
-        bulletEnemy.y += 10;
-    });
-}
-
-function collisions() {
-    for (var j = 0; j < enemys.length; j++) {
-
-        enemys[j].y += 3;
-
-        if (enemys[j].y >= 851) { //suppression enemy en dehors du canvas
-            stage.removeChild(enemys[j]);
-            enemys.splice(j, 1);
-        };
-
-        for (var k = 0; k < bullets.length; k++) { // collision tirs allies
-            if (typeof enemys[j] !== "undefined" && bullets[k].x >= enemys[j].x && bullets[k].x + 11 < enemys[j].x + 49 && bullets[k].y < enemys[j].y + 40) {
-
-                scoreUpdate();
-
-                stage.removeChild(enemys[j]); //suppresion de l'enemis et de la bullet qui lq touche`
-                enemys.splice(j, 1);
-
-                stage.removeChild(bullets[k]);
-                bullets.splice(k, 1);
-            };
-        };
-
-        for (var m = 0; m < bulletsEnemy.length; m++) { // collision tirs enemy
-
-            if (bulletsEnemy[m].x >= ship.x && bulletsEnemy[m].x + 11 < ship.x + 37 && bulletsEnemy[m].y > ship.y) {
-                //console.log('touche');
-                stage.removeChild(bulletsEnemy[m]); //supression de la bullet qui m'a touche
-                bulletsEnemy.splice(m, 1);
-
-                failed();
-            };
-            if (typeof bulletsEnemy[m] !== "undefined" && bulletsEnemy[m].y >= 851) {
-                stage.removeChild(bulletsEnemy[m]);
-                bulletsEnemy.splice(m, 1);
-            };
-        };
     };
 }
